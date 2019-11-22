c4c4b3522d1bc33c07c1f6d7b3860fe8af23c624
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-11-22 18:56:58 +0000

    Automatically backed up by Learn

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
+  @@all = []
+  attr_accessor :weight
+  attr_reader :species, :nickname
+
+
+  def initialize(species, weight, nickname)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def zoo
+
+  end
+
+  def self.find_by_species
+    @@all.select { |specie| animal.species == specie}
+  end
 
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,23 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+bronx_zoo = Zoo.new("Bronx Zoo","Bronx, NY")
+sdz = Zoo.new("San Diego Zoo","San Diego, CA")
+singapore = Zoo.new("Singapore Zoo","Singapore")
+berlin_zoo = Zoo.new("Zoo Berlin","Berlin, Germany")
+zsl = Zoo.new("ZSL London Zoo","London, England")
+
+
+kuzco = Animal.new("Guanaco", 200, "Kuzco")
+zaby = Animal.new("Pie Ruffed Lemur", 7, "Zaboomafoo")
+anga = Animal.new("Rhinoceros", 4000, "Angalifu")
+liwu = Animal.new("Giant Panda", 210, "Xiao Liwu")
+lenny = Animal.new("Fossa", 20, "Lenny")
+ah_meng = Animal.new("Orangutang", 150, "Ah Meng")
+nala = Animal.new("Lion", 280, "Nala")
+bambi = Animal.new("Springbok", 120, "Bambi")
+chi_chi = Animal.new("Giant Panda", 220, "Chi Chi")
+brumas = Animal.new("Polar Bear", 300, "Brumas")
 
 
 binding.pry
